301930bd5b2d45d05fdb9116ebe33243ef2698d6
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2019-01-12 21:30:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1836,4 +1836,30 @@ client.on('guildMemberAdd', member => {
 
 
 
+
+
+
+
+client.on('guildMemberAdd', member => {
+
+    const channel = member.guild.channels.get("531176512544833539");
+  
+    const millis = new Date().getTime() - member.user.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+  
+    const embed = new Discord.RichEmbed()
+    
+    .setColor("black")
+    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
+    .setAuthor(member.user.tag, member.user.avatarURL);
+    channel.sendEmbed(embed);
+});
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
