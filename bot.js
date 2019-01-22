951a4f0e60488dc81717f494694e60448df16897
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2019-01-22 21:11:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -411,7 +411,7 @@ client.on('guildMemberAdd', member => {
     const logChannel = member.guild.channels.find(channel => channel.name === `${welcome[member.guild.id].channel}`);
     if(!logChannel) return;
       setTimeout(() => {
-    logChannel.send(`Invited By: <@${inviter.id}>`);
+    logChannel.send(` ${member} Welocome To Imagine  , تم دعوته بواسطــة: <@${inviter.id}>`);
   },2000)
   });
 });
